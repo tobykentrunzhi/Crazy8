affd43a67d6813267d46b651655f87e21ac89cb9
tobykentrunzhi <396367738@qq.com>
2015-10-27 16:12:15 +0800

    Initial Commit

---
+++ Crazy8Tests/Crazy8Tests.m
@@ -0,0 +1,40 @@
+//
+//  Crazy8Tests.m
+//  Crazy8Tests
+//
+//  Created by Arron on 10/27/15.
+//  Copyright (c) 2015 Arron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Crazy8Tests : XCTestCase
+
+@end
+
+@implementation Crazy8Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
